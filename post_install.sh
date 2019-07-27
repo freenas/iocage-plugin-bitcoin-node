37e0154cc7d8445469c74569462c73c6a60b2555
Martin Wilke <miwi@ixsystems.com>
2019-07-28 00:24:47 +0800

    NAS-102660

--- post_install.sh
+++ post_install.sh
@@ -7,15 +7,15 @@ echo disablewallet=1 >> /usr/local/etc/bitcoin.conf
 
 service bitcoind start
 
-echo ""
-echo ""
-echo "bitcoin has hundreds of config options and there isn't a single mode of"
-echo "operation (mining, client, testnet etc)  that can be easily covered by a"
-echo "sample file."
-echo ""
-echo "A generator for some common configurations exists at"
-echo "https://jlopp.github.io/bitcoin-core-config-generator/"
-echo ""
-echo "The Current Database can be found at /var/db/bitcoin"
-echo ""
-echo ""
+echo "" > /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "bitcoin has hundreds of config options and there isn't a single mode of"  >> /root/PLUGIN_INFO
+echo "operation (mining, client, testnet etc)  that can be easily covered by a"  >> /root/PLUGIN_INFO
+echo "sample file."  >> /root/PLUGIN_INFO
+echo ""  >> /root/PLUGIN_INFO
+echo "A generator for some common configurations exists at"  >> /root/PLUGIN_INFO
+echo "https://jlopp.github.io/bitcoin-core-config-generator/"  >> /root/PLUGIN_INFO
+echo ""  >> /root/PLUGIN_INFO 
+echo "The Current Database can be found at /var/db/bitcoin"  >> /root/PLUGIN_INFO
+echo ""  >> /root/PLUGIN_INFO
+echo ""  >> /root/PLUGIN_INFO
